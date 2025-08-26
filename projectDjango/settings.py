87b712b42ca4d9e0ef8bc11b1ca8f372467f6033
Welliton Lima <welliton.s.lima1@gmail.com>
2025-08-26 20:07:28 -0300

    Adicionado load dotenv no settings.py

--- projectDjango/settings.py
+++ projectDjango/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 from django.contrib.messages import constants
 import os
 
+if os.environ.get('DEBUG',None) is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
