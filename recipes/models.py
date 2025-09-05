13ace371d19eb1dc843172047970df4ac5ba7d6b
Welliton Lima <welliton.s.lima1@gmail.com>
2025-09-04 22:35:21 -0300

    Commit ajustando a resolução de uma imagem carregado ao criar ou atualizar receitas

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.utils.text import slugify
 from collections import defaultdict
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from PIL import Image
+import os
 
 class Category(models.Model):
     name = models.CharField(max_length=65,unique=True,verbose_name=_('Category'))
@@ -40,12 +43,39 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse("recipes:recipe", kwargs={"id": self.id})
     
+    @staticmethod
+    def resize_image(image, new_width=840):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
-        return super().save(*args, **kwargs)
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 840)
+            except FileNotFoundError:
+                pass
+
+        return saved
     
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list) 
